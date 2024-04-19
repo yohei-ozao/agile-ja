4211e896c41627fc0c708d6fd3614ff96a4628e8
Yuma Sumi <yuma.sumi@woven-planet.global>
2024-04-19 11:52:41 +0900

    added 03 python version

---
+++ 03_version_control_advanced/01_challenge/python/tconv.py
@@ -0,0 +1,6 @@
+def convert_celsius_to_fahrenheit():
+    print("Enter the temperature in Celsius:")
+    celsius = input()
+    celsius = int(celsius)
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"The Celsius temperature {celsius} you entered is {fahrenheit} in Fahrenheit.")
